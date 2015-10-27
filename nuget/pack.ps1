d17e91f4fb4eaff188f81dab833b99e2916f4d3c
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-10-28 10:49:06 +1100

    Set the nuget spec to the build number

---
+++ nuget/pack.ps1
@@ -0,0 +1,8 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\St2.Client\bin\Release\St2.Client.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\St2.Client\St2.Client.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
