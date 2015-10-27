af037cd31fd8828e3732aa360a5e42b5cd7001ae
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-10-27 17:08:20 +1100

    Initialize the client connection from the session if not available.

--- St2.Client.PowerShell/BaseClientCmdlet.cs
+++ St2.Client.PowerShell/BaseClientCmdlet.cs
@@ -1,4 +1,5 @@
 ﻿using System.Management.Automation;
+using System.Security.Authentication;
 
 namespace TonyBaloney.St2.Client.PowerShell
 {
@@ -8,5 +9,27 @@ namespace TonyBaloney.St2.Client.PowerShell
 		[Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true,
 			HelpMessage = "The StackStorm Connection created by New-St2ClientConnection")]
 		public St2ClientConnection Connection { get; set; }
+
+		/// <summary>
+		///     The begin processing.
+		/// </summary>
+		protected override void BeginProcessing()
+		{
+			base.BeginProcessing();
+
+			// If CaaS connection is NOT set via parameter, get it from the PS session
+			if (Connection == null)
+			{
+				Connection = SessionState.GetDefaultServiceConnection();
+				if (Connection == null)
+					ThrowTerminatingError(
+						new ErrorRecord(
+							new AuthenticationException(
+								"Cannot find a valid connection. Use New-St2ClientConnection to create or Set-St2ActiveConnection to set a valid connection"),
+							"-1",
+							ErrorCategory.AuthenticationError,
+							this));
+			}
+		}
 	}
 }
