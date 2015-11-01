c119530d4dd1e532eabdf117455e3f8ff6035a93
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-02 10:08:47 +1100

    Script for generating MD for the PowerShell commands

---
+++ DocumentPowerShell.ps1
@@ -0,0 +1,8 @@
+Import-Module .\St2.Client.PowerShell\bin\Release\St2.Client.PowerShell.dll
+$commands = Get-Command -Module St2.Client.PowerShell 
+
+if((Test-Path ".\docs\powershell\") -eq 0){
+    mkdir ".\docs\powershell\"}
+foreach ($command in $commands){
+	.\Get-HelpByMarkdown.ps1 $command.name > .\docs\powershell\$command.md
+}
