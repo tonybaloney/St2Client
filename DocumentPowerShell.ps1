77793652a7ee578c95e3bbcf03124c9883a7785c
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-02 14:50:13 +1100

    If you can just go an do this, that'd be great appveyor. go WMF5!

--- DocumentPowerShell.ps1
+++ DocumentPowerShell.ps1
@@ -1,3 +1,7 @@
+param ( 
+ [string] $apiKey
+)
+
 Import-Module .\St2.Client.PowerShell\bin\Release\St2.Client.PowerShell.dll
 $commands = Get-Command -Module St2.Client.PowerShell 
 
@@ -6,3 +10,5 @@ if((Test-Path ".\docs\powershell\") -eq 0){
 foreach ($command in $commands){
 	.\Get-HelpByMarkdown.ps1 $command.name | Set-Content -Encoding utf8 .\docs\powershell\$command.md
 }
+
+Publish-Module -Name St2.Client.PowerShell -NuGetApiKey $apiKey
