f1a1be918a0b67659c91ac15e018926b811a836b
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-02 15:15:29 +1100

    thanks to Get-Help and Windows 10 for some help

--- DocumentPowerShell.ps1
+++ DocumentPowerShell.ps1
@@ -2,8 +2,8 @@ param (
  [string] $apiKey
 )
 
-Import-Module .\St2.Client.PowerShell\bin\Release\St2.Client.PowerShell.dll
-$commands = Get-Command -Module St2.Client.PowerShell 
+Import-Module .\St2.Client.PowerShell\bin\Release\St2.Client.psd1
+$commands = Get-Command -Module St2.Client
 
 if((Test-Path ".\docs\powershell\") -eq 0){
     mkdir ".\docs\powershell\"}
@@ -11,4 +11,4 @@ foreach ($command in $commands){
 	.\Get-HelpByMarkdown.ps1 $command.name | Set-Content -Encoding utf8 .\docs\powershell\$command.md
 }
 
-Publish-Module -Name St2.Client.PowerShell -NuGetApiKey $apiKey
+Publish-Module -Path .\St2.Client.PowerShell\bin\Release\St2.Client.psd1 -NuGetApiKey $apiKey
