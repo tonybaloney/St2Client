2fa0a3a8a0826644dde98c5a474bbe9b0040925d
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-02 16:09:16 +1100

    JFDI

--- DocumentPowerShell.ps1
+++ DocumentPowerShell.ps1
@@ -10,8 +10,22 @@ if((Test-Path ".\docs\powershell\") -eq 0){
 foreach ($command in $commands){
 	.\Get-HelpByMarkdown.ps1 $command.name | Set-Content -Encoding utf8 .\docs\powershell\$command.md
 }
+
+# put nuget-anycpu somewhere this module can find it
 $modPath = $Env:PSModulePath.split(';')[1]
 mkdir "$($modPath)\St2.Client"
+
+if ((Test-Path "C:\Program Files\PackageManagement") -eq 0){
+	mkdir "C:\Program Files\PackageManagement"
+}
+if ((Test-Path "C:\Program Files\PackageManagement\ProviderAssemblies") -eq 0){
+	mkdir "C:\Program Files\PackageManagement\ProviderAssemblies"
+}
+
 Copy-Item .\nuget\nuget-anycpu.exe "C:\Program Files\PackageManagement\ProviderAssemblies\"
+
+# Install the module
 Copy-Item .\St2.Client.PowerShell\bin\Release\*.* "$($modPath)\St2.Client"
+
+# publish the module
 Publish-Module -Name St2.Client -NuGetApiKey $apiKey
