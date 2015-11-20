b0649b91f073e0d91f93648eeb95d5c99304d3ce
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-20 11:43:34 +0800

    ..

--- DocumentPowerShell.ps1
+++ DocumentPowerShell.ps1
@@ -10,6 +10,10 @@ if((Test-Path ".\docs\powershell\") -eq 0){
 foreach ($command in $commands){
 	.\Get-HelpByMarkdown.ps1 $command.name | Set-Content -Encoding utf8 .\docs\powershell\$command.md
 }
+$modPath = $Env:PSModulePath.split(';')[1]
+mkdir "$($modPath)\St2.Client"
+# Install the module
+Copy-Item .\St2.Client.PowerShell\bin\Release\*.* "$($modPath)\St2.Client" -Verbose
 
 # publish the module
-Publish-Module -NuGetApiKey $apiKey -Path .\St2.Client.PowerShell\bin\Release\
+Publish-Module -NuGetApiKey $apiKey -Path "$($modPath)\St2.Client"
