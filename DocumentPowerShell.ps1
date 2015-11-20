54f3e4094a1d9ee3fd12617e9f6245db0ceecc3b
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-11-20 11:38:20 +0800

    simplify the packaging process

--- DocumentPowerShell.ps1
+++ DocumentPowerShell.ps1
@@ -11,21 +11,5 @@ foreach ($command in $commands){
 	.\Get-HelpByMarkdown.ps1 $command.name | Set-Content -Encoding utf8 .\docs\powershell\$command.md
 }
 
-# put nuget-anycpu somewhere this module can find it
-$modPath = $Env:PSModulePath.split(';')[1]
-mkdir "$($modPath)\St2.Client"
-
-if ((Test-Path "C:\Users\appveyor\AppData\Local\PackageManagement") -eq 0){
-	mkdir "C:\Users\appveyor\AppData\Local\PackageManagement"
-}
-if ((Test-Path "C:\Users\appveyor\AppData\Local\PackageManagement\ProviderAssemblies") -eq 0){
-	mkdir "C:\Users\appveyor\AppData\Local\PackageManagement\ProviderAssemblies"
-}
-
-Copy-Item .\nuget\nuget-anycpu.exe "C:\Users\appveyor\AppData\Local\PackageManagement\ProviderAssemblies\NuGet-anycpu.exe" -Verbose
-
-# Install the module
-Copy-Item .\St2.Client.PowerShell\bin\Release\*.* "$($modPath)\St2.Client" -Verbose
-
 # publish the module
-Publish-Module -Name St2.Client -NuGetApiKey $apiKey -Confirm
+Publish-Module -NuGetApiKey $apiKey -Path .\St2.Client.PowerShell\bin\Release\
